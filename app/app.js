b792f4350c105671dcedffa3a0245ee6700ab3d9
SeoJaeSik <ziaktmxj@nate.com>
2023-08-09 18:02:53 +0900

    폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -19,11 +19,11 @@ const express = require('express')
 const app = express()
 
 // 앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 // 라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 app.use("/", home); // use -> 미들웨어를 등록해주는 메소드
  
 // app.get('/', function (req, res) {
